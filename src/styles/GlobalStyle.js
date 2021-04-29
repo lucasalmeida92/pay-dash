a701631ec52f2b4b5761d2bd544bec6735fe23be
Lucas Almeida Siqueira <lucasalmeida70@gmail.com>
2021-04-28 21:53:41 -0300

    first commit (CRA generated project + styled-components)

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export default {
+    colors: {
+        lightGrey: '#eee',
+        lightGrey2: '#959595',
+        lightGrey3: '#cac5c3',
+        grey: '#252525',
+        dark: '#14131d',
+    }
+}
