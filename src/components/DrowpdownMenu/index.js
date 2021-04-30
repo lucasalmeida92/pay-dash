ba8bbac39b749321fe071b16cf92639553852c79
Lucas Almeida Siqueira <lucasalmeida70@gmail.com>
2021-04-30 20:50:51 -0300

    Created DropdownMenu reusable component

---
+++ src/components/DrowpdownMenu/index.js
@@ -0,0 +1,76 @@
+import { useRef, useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  position: relative;
+`
+const Menu = styled.div`
+  position: absolute;
+  overflow: hidden;
+  top: calc(100% + 16px);
+  right: 0;
+  display: ${({ isOpen }) => isOpen ? 'block' : 'none'};
+  border-radius: 24px;
+  padding: 2px;
+  background-color: ${({ theme }) => theme.colors.dark};
+
+  li {
+    border-bottom: 1px solid ${({ theme }) => theme.colors.lightGrey2};
+
+    &:last-child {
+      border-bottom: none;
+    }
+
+    a {
+      display: block;
+      padding: 16px 48px 16px 28px;
+      color: ${({ theme }) => theme.colors.lightGrey3};
+      font-size: .8rem;
+      white-space: nowrap;
+      transition: all 200ms ease-out;
+
+      &:hover {
+        background-color: rgba(255,255,255, .1);
+        color: ${({ theme }) => theme.colors.lightGrey1};
+      }
+    }
+  }
+`
+
+function DropdownMenu({ openButton, children, onClickOpen }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const wrapperRef = useRef(null)
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [wrapperRef])
+
+  function handleClickOpen() {
+    setIsOpen(!isOpen)
+    onClickOpen && onClickOpen()
+  }
+
+  return (
+    <Wrapper ref={wrapperRef}>
+      <span onClick={handleClickOpen}>
+        {openButton}
+      </span>
+
+      <Menu isOpen={isOpen}>
+        { children }
+      </Menu>
+    </Wrapper>
+  )
+}
+
+export default DropdownMenu
--- src/containers/Layout/UserMenu.js
+++ src/containers/Layout/UserMenu.js
@@ -1,5 +1,5 @@
-import { useRef, useEffect, useState } from 'react'
 import styled from 'styled-components'
+import DropdownMenu from '../../components/DrowpdownMenu'
 
 const UserPicture = styled.img`
   cursor: pointer;
@@ -14,73 +14,21 @@ const UserPicture = styled.img`
     box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.grey};
   }
 `
-const Wrapper = styled.div`
-  position: relative;
-`
-const DropdownMenu = styled.div`
-  position: absolute;
-  overflow: hidden;
-  top: calc(100% + 16px);
-  right: 0;
-  display: ${({ isOpen }) => isOpen ? 'block' : 'none'};
-  border-radius: 24px;
-  padding: 2px;
-  background-color: ${({ theme }) => theme.colors.dark};
 
-  li {
-    border-bottom: 1px solid ${({ theme }) => theme.colors.lightGrey2};
-
-    &:last-child {
-      border-bottom: none;
-    }
-
-    a {
-      display: block;
-      padding: 16px 48px 16px 28px;
-      color: ${({ theme }) => theme.colors.lightGrey3};
-      font-size: .8rem;
-      white-space: nowrap;
-      transition: all 200ms ease-out;
-
-      &:hover {
-        background-color: rgba(255,255,255, .1);
-        color: ${({ theme }) => theme.colors.lightGrey1};
-      }
-    }
-  }
-`
 
 function UserMenu() {
-  const [isOpen, setIsOpen] = useState(false)
-  const wrapperRef = useRef(null)
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-        setIsOpen(false)
-      }
-    }
-
-    document.addEventListener('mousedown', handleClickOutside)
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [wrapperRef])
-
   return (
-    <Wrapper ref={wrapperRef}>
-      <UserPicture
-        src="/images/user-picture.jpg"
-        alt="Fulano da Silva"
-        onClick={() => setIsOpen(!isOpen)}
-        isOpen={isOpen}
-      />
-      <DropdownMenu isOpen={isOpen}>
-        <li><a href="#" title="My account link">My account</a></li>
-        <li><a href="#" title="Loggout link">Loggout</a></li>
-      </DropdownMenu>
-    </Wrapper>
+    <DropdownMenu
+      openButton={
+        <UserPicture
+          src="/images/user-picture.jpg"
+          alt="Fulano da Silva"
+        />
+      }
+    >
+      <li><a href="#" title="My account link">My account</a></li>
+      <li><a href="#" title="Loggout link">Loggout</a></li>
+    </DropdownMenu>
   )
 }
 
